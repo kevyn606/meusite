6ed5e3b418adebd3737ff4495f3506bc2496ac48
kevyn 60 <kevynsousa606@gmail.com>
2023-10-04 23:58:23 -0300

    tirando

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,9 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -101,8 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
